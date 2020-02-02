b2d9e00df2505fe6580fbbb8ef90b50ae1ed969d
doong-jo <sdong001@gmail.com>
2020-02-02 23:35:17 +0900

    refactor: setPage dispatcher usage

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -3,22 +3,18 @@ import React, { useState, useEffect } from "react";
 import { useTransition, useSpring, animated as a } from "react-spring/three";
 
 import Shape from "./Shape";
+import SceneController from "../utils/sceneController";
 import { colors } from "../constants/arrays";
 import { paths } from "../utils/svg";
 
 function Scene() {
   const [page, setPage] = useState(0);
   const [shapes, setShapes] = useState([]);
-  // Switches scenes every 4 seconds
-  useEffect(
-    () => void setInterval(() => setPage(i => (i + 1) % paths.length), 3500),
-    []
-  );
-  // Converts current SVG into mesh-shapes: https://threejs.org/docs/index.html#examples/loaders/SVGLoader
+
+  SceneController.setPageDispatcher(setPage);
   useEffect(() => void paths[page].then(setShapes), [page]);
-  // This spring controls the background color animation
+
   const { color } = useSpring({ color: colors[page] });
-  // This one is like a transition group, but instead of handling div's it mounts/unmounts meshes in a fancy way
   const transitions = useTransition(shapes, item => item.shape.uuid, {
     from: {
       rotation: [0.0, -Math.PI / 4, 0],
