340e4c264f20dd0b1d2fc54d1640468732dbea58
doong-jo <sdong001@gmail.com>
2020-02-02 05:10:05 +0000

    Deploying to gh-pages from  - afa1fc08978936941b7fafba2d010d56e7028692 🚀

---
+++ precache-manifest.3735126f4fa3d9e5aa50105facbc9275.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "732e1e193d86a6847df166463ad3bb22",
+    "url": "/irony-mechanical-analysis/index.html"
+  },
+  {
+    "revision": "ad2980b3317ad03ec310",
+    "url": "/irony-mechanical-analysis/static/css/main.c5030efc.chunk.css"
+  },
+  {
+    "revision": "6988116afb658815fdc5",
+    "url": "/irony-mechanical-analysis/static/js/2.2368da2e.chunk.js"
+  },
+  {
+    "revision": "b62a14e84e3ceb73d9d1bd8b3c170605",
+    "url": "/irony-mechanical-analysis/static/js/2.2368da2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad2980b3317ad03ec310",
+    "url": "/irony-mechanical-analysis/static/js/main.a5270ae2.chunk.js"
+  },
+  {
+    "revision": "0d7de138e601a9f2098d",
+    "url": "/irony-mechanical-analysis/static/js/runtime-main.f7bf826b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4676832b45492ba40f771c5823cb61c9.js"
+  "/irony-mechanical-analysis/precache-manifest.3735126f4fa3d9e5aa50105facbc9275.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/irony-mechanical-analysis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
