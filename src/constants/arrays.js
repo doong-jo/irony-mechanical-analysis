c12dd88e35e88c12e0300d9791ba1e2d98689804
doong-jo <sdong001@gmail.com>
2020-02-08 19:16:17 +0900

    feat: add more colors and svgs

--- src/constants/arrays.js
+++ src/constants/arrays.js
@@ -4,14 +4,26 @@ export const colors = [
   "#ea5158",
   "#0d4663",
   "#2d4a3e",
+  "#8bd8d2",
+  "#ffbcb7",
+  "#21242d",
+  "#ea5158",
+  "#0d4663",
+  "#2d4a3e",
   "#8bd8d2"
 ];
 
 export const svgs = [
-  "happy",
-  "sad",
-  "angry",
-  "surprise",
-  "calm",
-  "anticipated"
+  "happy1",
+  "happy2",
+  "sad1",
+  "sad2",
+  "angry1",
+  "angry2",
+  "surprise1",
+  "surprise2",
+  "calm1",
+  "calm2",
+  "anticipated1",
+  "anticipated2"
 ];
