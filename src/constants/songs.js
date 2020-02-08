55f7ccd1a33bddaa9c83378de99ac9b73fcdec40
doong-jo <sdong001@gmail.com>
2020-02-08 21:49:29 +0900

    feat: add more songs

--- src/constants/songs.js
+++ src/constants/songs.js
@@ -13,7 +13,160 @@ export default [
     ]
   },
   {
-    name: "섬집아가",
-    sentences: [{ content: "섬집아가 가사 테스트", length: 2000 }]
+    name: "섬집아기",
+    sentences: [
+      { content: "엄마가 섬그늘에 굴따러가면", length: 12000 },
+      { content: "아이는 혼자남아 집을보다가", length: 12000 },
+      { content: "바다가 들려주는 자장노래에", length: 12000 },
+      { content: "팔베고 스르르르 잠이듭니다.", length: 12000 },
+      { content: "아기는 잠을곤히 자고있지만", length: 12000 },
+      { content: "갈매기 울음소리 맘이설레어", length: 12000 },
+      { content: "다못찬 굴바구니 머리에이고", length: 12000 },
+      { content: "엄마는 고갯길을 달려옵니다", length: 12000 }
+    ]
+  },
+  {
+    name: "Feeling Good",
+    sentences: [
+      { content: "Birds flying high", length: 3000 },
+      { content: "You know how I feel", length: 6000 },
+      { content: "Sun in the sky", length: 3000 },
+      { content: "You know how I feel", length: 6000 },
+      { content: "Breeze driftin' on by", length: 5000 },
+      { content: "You know how I feel", length: 3000 },
+      { content: "It's a new dawn", length: 3000 },
+      { content: "It's a new day", length: 2000 },
+      { content: "It's a new life For me ", length: 10000 },
+      { content: "And I'm feeling good", length: 9000 },
+      { content: "Fish in the sea", length: 3000 },
+      { content: "You know how I feel", length: 6000 },
+      { content: "River running free", length: 3000 },
+      { content: "You know how I feel", length: 6000 },
+      { content: "Blossom on a tree", length: 5000 },
+      { content: "You know how I feel", length: 3000 },
+      { content: "It's a new dawn", length: 3000 },
+      { content: "It's a new day", length: 2000 },
+      { content: "It's a new life For me ", length: 10000 },
+      { content: "And I'm feeling good", length: 8000 },
+      {
+        content:
+          "Dragonfly out in the sun, you know what I mean, don't you know",
+        length: 6000
+      },
+      {
+        content: "Butterflies all havin' fun, you know what I mean",
+        length: 6000
+      },
+      {
+        content: "Sleep in peace when day is done, that's what I mean",
+        length: 6000
+      },
+      { content: "And this old world is a new world", length: 3000 },
+      { content: "And a bold world For me", length: 8000 },
+      { content: "For me", length: 4000 },
+      { content: "Stars when you shine", length: 3000 },
+      { content: "You know how I feel", length: 3000 },
+      { content: "Scent of the pine", length: 3000 },
+      { content: "You know how I feel", length: 3000 },
+      { content: "Oh, freedom is mine", length: 3000 },
+      { content: "And I know how I feel", length: 3000 },
+      { content: "It's a new dawn", length: 1500 },
+      { content: "It's a new day", length: 1500 },
+      { content: "It's a new life", length: 3000 },
+      { content: "It's a new dawn", length: 1500 },
+      { content: "It's a new day", length: 1500 },
+      { content: "It's a new life", length: 3000 },
+      { content: "It's a new dawn", length: 1500 },
+      { content: "It's a new day", length: 1500 },
+      { content: "It's a new life", length: 2000 },
+      { content: "It's a new life", length: 2000 },
+      { content: "For me", length: 2000 },
+      { content: "And I'm feeling good", length: 8000 },
+      { content: "I'm feeling good", length: 7000 },
+      { content: "I'm feeling so good", length: 5000 },
+      { content: "I feel so good", length: 10000 }
+    ]
+  },
+  {
+    name: "Easy Life",
+    sentences: [
+      { content: "And when it rains, it rains", length: 1000 },
+      { content: "Orange juice and pink lemonade", length: 2000 },
+      { content: "In that 4 weeks she brings her A game", length: 3000 },
+      {
+        content: "Is it any wonder why I've been feeling so fucked up?",
+        length: 5000
+      },
+      { content: "And when it rains, it rains", length: 3000 },
+      { content: "Orange juice and pink lemonade", length: 3000 },
+      {
+        content: "To Champagne tears falling heavy on my suede shoes",
+        length: 3000
+      },
+      { content: "She left me feeling real fucked up", length: 9000 },
+      { content: "Sexy missionary from the east", length: 2000 },
+      { content: "She paints like Picasso when", length: 3000 },
+      { content: "And digs jazzy beats", length: 1000 },
+      { content: "Sips Guinness, Guru spinning on repeat", length: 3000 },
+      { content: "These minutes into hours", length: 2000 },
+      { content: "And these days into weeks", length: 1000 },
+      { content: "She stays pretty overthinking her speech", length: 4000 },
+      { content: "But there ain't nobody else that dare speak", length: 3000 },
+      { content: "Just don't mess with her parameter", length: 2000 },
+      { content: "Her old mans in her the mafia", length: 2000 },
+      { content: "You're a stone cold killer", length: 1000 },
+      { content: "These Englishmen are amateurs", length: 4000 },
+      { content: "And when it rains, it rains", length: 2000 },
+      { content: "Orange juice and pink lemonade", length: 2000 },
+      { content: "In that 4 weeks she brings her A game", length: 4000 },
+      {
+        content: "Is it any wonder why I've been feeling so fucked up?",
+        length: 3000
+      },
+      { content: "And when it rains, it rains", length: 4000 },
+      { content: "Orange juice and pink lemonade", length: 2000 },
+      {
+        content: "To champange tears falling heavy on my suede shoes",
+        length: 4000
+      },
+      { content: "She left me feeling real fucked up", length: 4000 },
+      { content: "La la la la la la la la la la la", length: 4000 },
+      { content: "La la la la la la la la la la la", length: 4000 },
+      { content: "La la la la la la la la la la la", length: 4000 },
+      { content: "Well, it could be better could be worse", length: 3000 },
+      { content: "This mediocrity is just the least I deserve", length: 3000 },
+      { content: "Because i've been putting in the work", length: 3000 },
+      { content: "But does a strong work ethic actually worth?", length: 3000 },
+      {
+        content: "Sometimes I feel I'm selling seeds just to feed the birds",
+        length: 3000
+      },
+      {
+        content: "And these pearlescent pigeons come and scratch up my turf",
+        length: 3000
+      },
+      { content: "But what's my better nature worth", length: 3000 },
+      {
+        content: "Because I give a little something but get nothing in return",
+        length: 3000
+      },
+      { content: "And when it rains, it rains", length: 2000 },
+      { content: "Orange juice and pink lemonade", length: 2000 },
+      { content: "In that 4 weeks she brings her A game", length: 3000 },
+      {
+        content: "Is it any wonder why I've been feeling so fucked up?",
+        length: 5000
+      },
+      { content: "And when it rains, it rains", length: 2000 },
+      { content: "Orange juice and pink lemonade", length: 2000 },
+      {
+        content: "To champange tears falling heavy on my suede shoes",
+        length: 4000
+      },
+      { content: "She left me feeling real fucked up", length: 4000 },
+      { content: "La la la la la la la la la la la", length: 4000 },
+      { content: "La la la la la la la la la la la", length: 4000 },
+      { content: "La la la la la la la la la la la", length: 4000 }
+    ]
   }
 ];
