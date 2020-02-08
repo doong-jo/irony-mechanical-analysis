a8d5c9ce13987994e615fb1dc3d11b66220682a5
doong-jo <sdong001@gmail.com>
2020-02-08 10:01:36 +0000

    Deploying to gh-pages from  - aecdb8c309662cab947d7beb601b4e1db94fd421 🚀

---
+++ precache-manifest.1987b50a62d0198294b6da3d553a2030.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8617edee88267620f363aef64caedd",
+    "url": "/irony-mechanical-analysis/index.html"
+  },
+  {
+    "revision": "07bbaaed6de7736b1c72",
+    "url": "/irony-mechanical-analysis/static/css/main.c5030efc.chunk.css"
+  },
+  {
+    "revision": "7a4f35a63717c1e26cd4",
+    "url": "/irony-mechanical-analysis/static/js/2.fcde0146.chunk.js"
+  },
+  {
+    "revision": "2f867c277c9f2e7853de96e921e41d80",
+    "url": "/irony-mechanical-analysis/static/js/2.fcde0146.chunk.js.LICENSE"
+  },
+  {
+    "revision": "07bbaaed6de7736b1c72",
+    "url": "/irony-mechanical-analysis/static/js/main.55f0b4d0.chunk.js"
+  },
+  {
+    "revision": "0d7de138e601a9f2098d",
+    "url": "/irony-mechanical-analysis/static/js/runtime-main.f7bf826b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/irony-mechanical-analysis/precache-manifest.6476d692afff01654c8cf0cd1c3b40bb.js"
+  "/irony-mechanical-analysis/precache-manifest.1987b50a62d0198294b6da3d553a2030.js"
 );
 
 self.addEventListener('message', (event) => {
