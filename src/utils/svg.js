8ec75519dd00743d3a7911376c91229173faaa20
doong-jo <sdong001@gmail.com>
2020-02-08 21:19:57 +0900

    refactor: use group id in svg

--- src/utils/svg.js
+++ src/utils/svg.js
@@ -14,27 +14,18 @@ export const paths = svgs
   .map(
     url =>
       new Promise(resolve =>
-        new loader().load(url, shapes => {
-          let indexId = 0;
-          let befId = 0;
-
-          return resolve(
+        new loader().load(url, shapes =>
+          resolve(
             flatten(
-              shapes.paths.map(group => {
-                const curId = group.userData.node.id;
-                if (curId === "" || curId !== befId) {
-                  indexId++;
-                }
-                befId = curId;
-
-                return group.toShapes(true).map(shape => ({
+              shapes.paths.map(group =>
+                group.toShapes(true).map(shape => ({
                   shape,
                   color: group.color,
-                  index: indexId
-                }));
-              })
+                  index: group.userData.node.id
+                }))
+              )
             )
-          );
-        })
+          )
+        )
       )
   );
