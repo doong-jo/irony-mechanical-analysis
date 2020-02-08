7c1e7cf000dbb780ec1b100ab02df96318f9f35f
doong-jo <sdong001@gmail.com>
2020-02-08 21:06:27 +0900

    feat: compound path id

--- src/utils/svg.js
+++ src/utils/svg.js
@@ -5,8 +5,8 @@ import { svgs } from "../constants/arrays";
 
 export const paths = svgs
   .map(
-    name =>
-      `https://raw.githubusercontent.com/doong-jo/irony-mechanical-analysis/master/public/images/svg/sad1.svg`
+    name => `./images/svg/sad1.svg`
+    // `https://raw.githubusercontent.com/doong-jo/irony-mechanical-analysis/master/public/images/svg/sad1.svg`
     // name =>
     //   `https://raw.githubusercontent.com/doong-jo/irony-mechanical-analysis/master/src/resources/images/svg/${name +
     //     (Math.random() > 0.5 ? 1 : 2)}.svg`
@@ -14,16 +14,27 @@ export const paths = svgs
   .map(
     url =>
       new Promise(resolve =>
-        new loader().load(url, shapes =>
-          resolve(
+        new loader().load(url, shapes => {
+          let indexId = 0;
+          let befId = 0;
+
+          return resolve(
             flatten(
-              shapes.paths.map((group, index) =>
-                group
-                  .toShapes(true)
-                  .map(shape => ({ shape, color: group.color, index }))
-              )
+              shapes.paths.map(group => {
+                const curId = group.userData.node.id;
+                if (curId === "" || curId !== befId) {
+                  indexId++;
+                }
+                befId = curId;
+
+                return group.toShapes(true).map(shape => ({
+                  shape,
+                  color: group.color,
+                  index: indexId
+                }));
+              })
             )
-          )
-        )
+          );
+        })
       )
   );
