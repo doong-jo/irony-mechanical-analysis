6a9a1628a0f9c22084b46d689af2f1850f8e82de
doong-jo <sdong001@gmail.com>
2020-02-08 22:31:02 +0900

    fix: not undefined id (is empty string)

--- src/utils/svg.js
+++ src/utils/svg.js
@@ -3,7 +3,7 @@ import flatten from "lodash-es/flatten";
 
 import { svgs } from "../constants/arrays";
 
-const testSvgName = "sad1";
+const testSvgName = "sadness_2_n";
 
 export const paths = svgs
   .map(name => `/${testSvgName}.svg`)
@@ -12,13 +12,13 @@ export const paths = svgs
       new Promise(resolve =>
         new loader().load(url, shapes => {
           let indexId = 0;
-          let befId = 0;
+          let befId = "";
 
           return resolve(
             flatten(
               shapes.paths.map(group => {
                 const curId = group.userData.node.id;
-                if (curId !== undefined && curId !== befId) {
+                if (curId !== "" && curId !== befId) {
                   indexId++;
                   befId = curId;
                 }
